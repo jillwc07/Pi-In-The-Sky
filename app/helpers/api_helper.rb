3c65ba977c30c7b1e4becf080e2cc7f8b881a9d5
Jillian Campbell <jill.campbell07@gmail.com>
2016-09-30 14:31:44 -0400

    Add new AQI key

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -1,7 +1,7 @@
 module ApiHelper
 
   def call_breezy_api(object)
-    url = "https://api.breezometer.com/baqi/?location=#{object.city},+#{object.state}&key=c0bfb33a27924f7e95a828abc931d5a0"
+    url = "https://api.breezometer.com/baqi/?location=#{object.city},+#{object.state}&key=d2ebdbd341d347deaf72c219131ed4a8"
     # binding.pry
     if url.ascii_only?
       party_url = HTTParty.get(url, :verify => false)
